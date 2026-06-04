6080a9298758ebe9d74d8c71e90b529423ecd6bf
otake-isuke <keitarouohta@gmail.com>
2026-06-04 16:35:26 +0900

    29 1005

--- navLink.js
+++ navLink.js
@@ -1,9 +1,9 @@
 const navHTML = `
     <ul>
         <li><a href="index.html">ホーム</a></li>
-        <li><a href="どうでも良い記事.html">どうでも良い記事</a></li>
-        <li><a href="日記.html">日記</a></li>
-        <li><a href="その他.html" >その他</a></li>
+        <li><a href="article.html">どうでも良い記事</a></li>
+        <li><a href="diary.html">日記</a></li>
+        <li><a href="others.html" >その他</a></li>
     </ul>
 `;
 document.getElementById('navLink').innerHTML = navHTML;
