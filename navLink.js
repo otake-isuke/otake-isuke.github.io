931833844303709b444c8cea6f87d5e8ea29987f
otake-isuke <keitarouohta@gmail.com>
2026-06-04 18:49:57 +0900

    change name of the article

--- navLink.js
+++ navLink.js
@@ -1,7 +1,7 @@
 const navHTML = `
     <ul>
         <li><a href="index.html">ホーム</a></li>
-        <li><a href="article.html">どうでも良い記事</a></li>
+        <li><a href="article.html">記事</a></li>
         <li><a href="diary.html">日記</a></li>
         <li><a href="others.html">その他</a></li>
     </ul>
