e475c785afc9afd3553c2e52fe3eef474424b6b0
otake-isuke <keitarouohta@gmail.com>
2026-08-13 10:53:14 +0900

    separate tex macro

---
+++ texMacro.js
@@ -0,0 +1,42 @@
+window.MathJax = {
+    chtml: {
+    matchFontHeight: false /* 👈 周りの文字の高さに無理に合わせる機能をオフ */
+    },
+    svg: {
+    matchFontHeight: false
+    },
+    tex: {
+        macros: {
+        // \newcommand{\R}{\mathbb{R}} と同じ
+        R: "{\\mathbb{R}}",
+        C: "{\\mathbb{C}}",
+        Z: "{\\mathbb{Z}}",
+        Q: "{\\mathbb{Q}}",
+        N: "{\\mathbb{N}}",
+        
+        dis: "{\\displaystyle}",
+        bra: ["{\\left(#1\\right)}",1],
+        
+        L: "{\\mathcal{L}}",
+        M: "{\\mathcal{M}}",
+        S: "{\\mathcal{S}}",
+        F: "{\\mathcal{F}}",
+
+        a: "{\\alpha}",
+        d: "{\\delta}",
+        e: "{\\varepsilon}",
+        wwidehat: ["{\\widehat{\\, #1 \\ }}", 1],
+        wwidetilde: ["{\\widetilde{\\, #1 \\ }}", 1],
+        
+        // 引数を取る場合：\newcommand{\vector}[1]{\mathbf{#1}} と同じ
+        // [マクロ名, 定義, 引数の数] の順に指定します
+        vec: ["{\\mathbf{#1}}", 1],
+        
+        // 既存のコマンドを組み合わせる例
+        set: ["{\\{ #1 \\}}", 1],
+
+        // \DeclareMathOperator{\tg}{tg} と同じ
+        supp: "{\\operatorname{supp}\\,}",
+        }
+    }
+};
